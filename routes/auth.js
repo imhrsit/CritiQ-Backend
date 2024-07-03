6521d78813aa5d3436e91491f458b54bbb9bb2a5
imhrsit <tiwariharsh1408@gmail.com>
2024-07-03 16:28:44 +0530

    mausam accha hai

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,7 @@ const authRouter = express.Router();
 const bcryptjs = require('bcryptjs');
 const User = require('../models/user');
 const jwt = require('jsonwebtoken');
+const auth = require('../middlewares/auth');
 
 //sign up routes
 authRouter.post("/api/signup", async (req, res) => {
@@ -42,6 +43,32 @@ authRouter.post("/api/login", async (req, res) => {
     } catch (e) {
         res.status(500).json({ error: e.message });
     }
+});
+
+authRouter.post("/tokenIsValid", async (req, res) => {
+    try {
+        const token = req.header("x-auth-token");
+        if (!token) {
+            return res.json(false);
+        }
+        const verified = jwt.verify(token, "passwordKey");
+        if (!verified) {
+            return res.json(false);
+        }
+        const user = await User.findById(verified.id);
+        if (!user) {
+            return res.json(false);
+        }
+        return res.json(true);
+    } catch (e) {
+        res.status(500).json({ error: e.message });
+    }
+});
+
+// get user data
+authRouter.get("/", auth, async (req, res) => {
+    const user = await User.findById(req.user);
+    res.json({ ...user._doc, token: req.token });
 });
 
 module.exports = authRouter; 
